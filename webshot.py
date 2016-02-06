b0bf307dc37764399cbd082357a5642996ea6e99
995306681@qq.com <995306681@qq.com>
2016-02-06 13:44:27 +0800

    modified sendmail function

--- webshot.py
+++ webshot.py
@@ -3,13 +3,18 @@
 import time
 import string
 from selenium import webdriver
+
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+from email.mime.base import MIMEBase
+from email import encoders
 
-#sites = [{'name':'www','url':'http://www.simuwang.com'},{'name':'pt','url':'http://pt.simuwang.com'},{'name':'news','url':'http://news.simuwang.com'},{'name':'dc','url':'http://dc.simuwang.com'},{'name':'mall','url':'http://mall.simuwang.com'},{'name':'m','url':'http://m.simuwang.com'},{'name':'mobile','url':'http://mobile.simuwang.com'}]
-sites = [{'name':'www','url':'http://www.simuwang.com'}]
+import os
+
+sites = [{'name':'www','url':'http://www.simuwang.com'},{'name':'pt','url':'http://pt.simuwang.com'},{'name':'news','url':'http://news.simuwang.com'},{'name':'dc','url':'http://dc.simuwang.com'},{'name':'mall','url':'http://mall.simuwang.com'},{'name':'m','url':'http://m.simuwang.com'},{'name':'mobile','url':'http://mobile.simuwang.com'}]
+#sites = [{'name':'www','url':'http://www.simuwang.com'}]
 idfile = "./logs/id"
 logifle = "./logs/check-logs.txt"
 imagepath = "./images/"
@@ -34,12 +39,12 @@ for site in sites:
     #browser.set_window_size(1024, 800)
     imgname = imagepath + site['name']+"_"+id+".png"
     browser.get(site['url'])
-    time.sleep(2)
     browser.save_screenshot(imgname)
     filelist.append(imgname)
     logtext = "第"+id+"次检查"+site['name']+"站点完成\r\n";
     print(logtext)
     log += logtext;
+    time.sleep(1)
 browser.quit()
 log += "//-----------------------------------------------------------\r\n"
 
@@ -65,10 +70,14 @@ def smail(title,files):
     
    
     for file in files:
-        msg.attach(addimg(file,"file_"+str(fno)))
-        fno +=1
-        imgcontent += "<img src=\"cid:"+"file_"+str(fno)+"\" border=\"1\"><br />"
-    msgtext = MIMEText("<font color=red>私募排排网网站截图:<br>"+imgcontent+"详细内容见附件。</font>","html","utf-8")
+        #msg.attach(addimg(file,"file_"+str(fno)))
+        #fno +=1
+        part = MIMEBase('application', 'octet-stream') #'octet-stream': binary data
+        part.set_payload(open(file, 'rb').read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(file))
+        msg.attach(part)
+    msgtext = MIMEText("<font color=red>私募排排网网站截图:<br>详细内容见附件。</font>","html","utf-8")
     msg.attach(msgtext)
     #attach = MIMEText(open("doc/week_report.xlsx", "rb").read(), "base64", "utf-8")
     #attach["Content-Type"] = "application/octet-stream"
@@ -80,11 +89,11 @@ def smail(title,files):
     msg['To']=TO
     try:
         server = smtplib.SMTP()
-        server.set_debuglevel(1)
+        #server.set_debuglevel(1)
         server.connect(HOST,"25")
         #server.docmd("EHLO server")
         #server.starttls()
-        server.login("cgx2625526","chguoxi33651")
+        server.login("","")
         server.sendmail(FROM, TO, msg.as_string())
         server.quit()
         print("邮件发送成功！")
