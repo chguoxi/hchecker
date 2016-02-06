cd78ad00896dc109b569415f50582aaa7b435fd5
995306681@qq.com <995306681@qq.com>
2016-02-06 11:37:05 +0800

    modified send maill function

--- webshot.py
+++ webshot.py
@@ -3,12 +3,17 @@
 import time
 import string
 from selenium import webdriver
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
 #sites = [{'name':'www','url':'http://www.simuwang.com'},{'name':'pt','url':'http://pt.simuwang.com'},{'name':'news','url':'http://news.simuwang.com'},{'name':'dc','url':'http://dc.simuwang.com'},{'name':'mall','url':'http://mall.simuwang.com'},{'name':'m','url':'http://m.simuwang.com'},{'name':'mobile','url':'http://mobile.simuwang.com'}]
 sites = [{'name':'www','url':'http://www.simuwang.com'}]
 idfile = "./logs/id"
 logifle = "./logs/check-logs.txt"
 imagepath = "./images/"
+filelist = []
 with open(idfile,"r") as f:
     id = f.read()
     if(id==''):
@@ -27,9 +32,11 @@ browser = webdriver.Firefox()
 log = "//-----------------------------------------------------------\r\n"
 for site in sites:
     #browser.set_window_size(1024, 800)
+    imgname = imagepath + site['name']+"_"+id+".png"
     browser.get(site['url'])
     time.sleep(2)
-    browser.save_screenshot(imagepath + site['name']+"_"+id+".png")
+    browser.save_screenshot(imgname)
+    filelist.append(imgname)
     logtext = "第"+id+"次检查"+site['name']+"站点完成\r\n";
     print(logtext)
     log += logtext;
@@ -42,45 +49,48 @@ with open(logifle,"a") as f:
     f.close()
 
 # 邮件
-#coding: utf-8
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-
-HOST = "smtp.qq.com"
-SUBJECT = u"官网业务服务质量周报"
-TO = "goss@lunluoren.com"
-FROM = "995306681@qq.com"
-
 def addimg(src,imgid):
     fp = open(src, 'rb')
     msgImage = MIMEImage(fp.read())
     fp.close()
     msgImage.add_header('Content-ID', imgid)
     return msgImage
-def smail(title,content,file):
+def smail(title,files):
+    HOST = "smtp.126.com"
+    TO = "goss@lunluoren.com"
+    FROM = "cgx2625526@126.com"
+    imgcontent = ''
+    fno = 1
     msg = MIMEMultipart('related')
-    msgtext = MIMEText("<font color=red>私募排排网网站截图:<br><img src=\"cid:weekly\" border=\"1\"><br>详细内容见附件。</font>","html","utf-8")
+    
+   
+    for file in files:
+        msg.attach(addimg(file,"file_"+str(fno)))
+        fno +=1
+        imgcontent += "<img src=\"cid:"+"file_"+str(fno)+"\" border=\"1\"><br />"
+    msgtext = MIMEText("<font color=red>私募排排网网站截图:<br>"+imgcontent+"详细内容见附件。</font>","html","utf-8")
     msg.attach(msgtext)
-    msg.attach(addimg(file,"weekly"))
     #attach = MIMEText(open("doc/week_report.xlsx", "rb").read(), "base64", "utf-8")
     #attach["Content-Type"] = "application/octet-stream"
     #attach["Content-Disposition"] = "attachment; filename=\"业务服务质量周报(12周).xlsx\"".decode("utf-8").encode("gb18030")
     #msg.attach(attach)
-    msg['Subject'] = SUBJECT
+
+    msg['Subject'] = title
     msg['From']=FROM
     msg['To']=TO
     try:
         server = smtplib.SMTP()
+        server.set_debuglevel(1)
         server.connect(HOST,"25")
-        server.starttls()
-        server.login("995306681@qq.com","chenguoxi33681")
+        #server.docmd("EHLO server")
+        #server.starttls()
+        server.login("cgx2625526","chguoxi33651")
         server.sendmail(FROM, TO, msg.as_string())
         server.quit()
         print("邮件发送成功！")
-    except Exception(e):  
-        print("失败："+str(e)) 
+    except Exception as e:  
+        print("失败："+str(e))
+smail("私募排排网站点截图",filelist)
 
 
 
